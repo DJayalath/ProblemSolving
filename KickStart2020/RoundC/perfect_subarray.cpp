eed89b6ae93a050a377eb1f04c69a945aacf5bd0
Dulhan Jayalath <dhjayalath@protonmail.com>
2020-05-21 18:59:12 +0100

    Improved to O(n^2)

--- KickStart2020/RoundC/perfect_subarray.cpp
+++ KickStart2020/RoundC/perfect_subarray.cpp
@@ -14,23 +14,48 @@ void solve() {
     cin >> N;
 
     int arr[N];
+    int t = 0;
+    int c[N];
+    int s_pos = 0;
+    int s_neg = 0;
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         cin >> arr[i];
+        t += arr[i];
+        c[i] = t;
+        if (arr[i] > 0)
+            s_pos += arr[i];
+        else
+            s_neg -= arr[i];
+    }
 
     int ans = 0;
-
-    // Loop over subarrays
     for (int i = 0; i < N; i++) {
-        for (int j = i; j < N; j++) {
-            int sum = 0;
-            for (int k = i; k <= j; k++) {
-                sum += arr[k];
-            }
-            if (p_square(sum))
+        bool good = true;
+        for (int j = 0; j < N - i && good; j++) {
+            int sum = c[i + j];
+            if (i > 0)
+                sum -= c[i - 1];
+            if (sum > s_pos)
+                good = false;
+            else if (p_square(sum)) {
                 ans++;
+            }
         }
     }
+
+
+    // Loop over subarrays
+   // for (int i = 0; i < N; i++) {
+   //     for (int j = i; j < N; j++) {
+   //         int sum = 0;
+   //         for (int k = i; k <= j; k++) {
+   //             sum += arr[k];
+   //         }
+   //         if (p_square(sum))
+   //             ans++;
+   //     }
+   // }
     
 
 
