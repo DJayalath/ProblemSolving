75ce2703c8e9a58d7d757d6d769fa5eecd1e3650
Dulhan Jayalath <dhjayalath@protonmail.com>
2020-05-21 20:47:40 +0100

    O(n) solution based on William Lin

--- KickStart2020/RoundC/perfect_subarray.cpp
+++ KickStart2020/RoundC/perfect_subarray.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int c[20000001]; // Frequency of totals
+
 bool p_square(int n) {
     if (n < 0)
         return false;
@@ -14,52 +16,39 @@ void solve() {
     cin >> N;
 
     int arr[N];
-    int t = 0;
-    int c[N];
     int s_pos = 0;
     int s_neg = 0;
 
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
-        t += arr[i];
-        c[i] = t;
         if (arr[i] > 0)
             s_pos += arr[i];
         else
             s_neg -= arr[i];
     }
 
-    int ans = 0;
+    long long ans = 0;
+    int t = 0;
+    c[s_neg]++;
     for (int i = 0; i < N; i++) {
-        bool good = true;
-        for (int j = 0; j < N - i && good; j++) {
-            int sum = c[i + j];
-            if (i > 0)
-                sum -= c[i - 1];
-            if (sum > s_pos)
-                good = false;
-            else if (p_square(sum)) {
-                ans++;
-            }
+
+        t += arr[i];
+
+        // examine subarrays with perfect square sum
+        // if subarray has sum j*j for array t then
+        // total sum to this point is (s_neg +) t - j*j
+        // (if indexable) add the count of this sum to number of answers
+        for (int j = 0; j*j <= s_pos; j++) {
+            if (s_neg + (t - j*j) >= 0)
+                ans += c[s_neg + (t - j*j)];
         }
+
+        c[s_neg + t]++; // Add one to frequency of this total
     }
 
-
-    // Loop over subarrays
-   // for (int i = 0; i < N; i++) {
-   //     for (int j = i; j < N; j++) {
-   //         int sum = 0;
-   //         for (int k = i; k <= j; k++) {
-   //             sum += arr[k];
-   //         }
-   //         if (p_square(sum))
-   //             ans++;
-   //     }
-   // }
-    
-
-
     cout << ans << endl;
+
+    memset(c, 0, sizeof(int) * (s_neg + s_pos + 1));
 
 }
 
