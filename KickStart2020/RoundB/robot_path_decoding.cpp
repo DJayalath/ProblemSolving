81f7c57cf0daa0711f3686554652f9048c8e07b7
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-05-23 17:47:06 +0100

    Improved to linear time - WA test set 2

--- KickStart2020/RoundB/robot_path_decoding.cpp
+++ KickStart2020/RoundB/robot_path_decoding.cpp
@@ -2,73 +2,14 @@
 using namespace std;
 
 #define ull unsigned long long int
-#define MAX 1000000000
+#define ll long long int
 
-ull w;
-ull h;
+ll MAX = 1000000000;
+ll w;
+ll h;
 
 string prog;
-
-
-void exec(string p, int repeats) {
-    
-    int rep;
-    bool bra = false;
-    int bal = 0;
-    string s = "";
-    //cout << p << endl;
-    
-    ull offset_w = 0;
-    ull offset_h = 0;
-
-    for (char c : p) {
-        if (!bra && c >= '2' && c <= '9')
-            rep = c - '0';
-        else if (c == '(' && !bra) {
-            bra = true;
-        } else if (c == '(' && bra) {
-            s += c;
-            bal += 1;
-        } else if (c == ')' && bra) {
-            if (bal == 0) {
-                exec(s, rep);
-                bra = false;
-                bal = 0;
-                s.clear();
-            } else {
-                s += c;
-                bal -= 1;
-            }
-        } else if (!bra) {
-            //cout << c;
-            if (c == 'N') {
-                offset_h -= 1;
-            } else if (c == 'S') {
-                offset_h += 1;
-            } else if (c == 'W') {
-                offset_w -= 1;
-            } else if (c == 'E') {
-                offset_w += 1;
-            }
-        } else {
-            s += c;
-        }
-    }
-
-    h += repeats * offset_h;
-    w += repeats * offset_w;
-
-    if (h > MAX)
-        h = h - MAX;
-    else if (h < 1)
-        h = MAX - h;
-
-    if (w > MAX)
-        w = w - MAX;
-    else if (w < 1)
-        w = MAX - w;
-
-}
+vector<ll> rstk; // Repetition stack
 
 void solve() {
     
@@ -77,7 +18,35 @@ void solve() {
 
     cin >> prog;
 
-    exec(prog, 1);
+    for (char c : prog) {
+        if (c >= '2' && c <= '9') {
+            ll r = c - '0';
+            rstk.push_back(r * rstk.back());
+        }
+        else if (c == ')') {
+            rstk.pop_back();
+        }
+        else if (c != '(') {
+            if (c == 'N')
+                h -= rstk.back();
+            else if (c == 'S')
+                h += rstk.back();
+            else if (c == 'W')
+                w -= rstk.back();
+            else if (c == 'E')
+                w += rstk.back();
+        }
+    }
+
+    if (h > MAX)
+        h = h - MAX;
+    else if (h < 1)
+        h = MAX + h;
+
+    if (w > MAX)
+        w = w - MAX;
+    else if (w < 1)
+        w = MAX + w;
 
     cout << w << " " << h << endl;
 
@@ -87,6 +56,8 @@ int main(void) {
 
     int c;
     cin >> c;
+
+    rstk.push_back(1);
 
     for (int i = 1; i <= c; i++) {
         cout << "Case #" << i << ": ";
