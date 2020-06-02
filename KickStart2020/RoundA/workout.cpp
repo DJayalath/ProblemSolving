4422d01c73a331acc19c4d2404eda60b87f92cf9
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-02 14:34:20 +0100

    Fixed and working using binary search log-lin time

--- KickStart2020/RoundA/workout.cpp
+++ KickStart2020/RoundA/workout.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ksum(int N, int d[], int d_opt) {
+
+    int s = 0;
+    for (int i = 0; i < N - 1; i++) {
+        s += ceil((double) d[i] / (double) d_opt) - 1;
+    }
+    return s;
+
+}
+
 void solve() {
 
     int N, K;
@@ -11,8 +21,32 @@ void solve() {
         cin >> a[i];
 
     int d[N - 1];
-    for (int i = 0; i < N - 1; i++)
+    int d_max = 0;
+    for (int i = 0; i < N - 1; i++) {
         d[i] = a[i + 1] - a[i];
+        if (d[i] > d_max)
+            d_max = d[i];
+    }
+
+    // Perform binary search
+    int mx = d_max;
+    int mn = 1;
+    int d_opt = (mx + mn) / 2;
+    while (true) {
+
+        if (ksum(N, d, d_opt) <= K) {
+            mx = d_opt;
+        } else {
+            mn = d_opt + 1;
+        }
+
+        if (mx == mn)
+            break;
+
+        d_opt = (mx + mn) / 2;
+    }
+
+    cout << mx << endl;
 
 }
 
